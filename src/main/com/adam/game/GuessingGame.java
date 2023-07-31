c16ffd814b65d2b0f049f5d0d1cd3781afe31cef
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-07-31 07:20:43 +0200

    Game engine implemented (with tests)

--- src/main/com/adam/game/GuessingGame.java
+++ src/main/com/adam/game/GuessingGame.java
@@ -1,12 +1,37 @@
 package com.adam.game;
 
+import java.util.Random;
+
 public class GuessingGame {
+    private final int randomNumber = new Random().nextInt(10) + 1;
+    private int numberOfGuesses = 0;
+
     public String guess(int guessedNumber) {
-        return (guessedNumber == getRandomNumber()) ? "You got it" : "You didn't get it";
+        numberOfGuesses++;
+
+        String tryText = numberOfGuesses > 1 ? "tries" : "try";
+        String winningMessage = String.format("You got it in %d %s", numberOfGuesses, tryText);
+        String response;
+
+        if (numberOfGuesses == 4 && guessedNumber != getRandomNumber()) {
+            response = String.format("You didn't get it and you've had %d %s. Game over.", numberOfGuesses, tryText);
+        } else if (numberOfGuesses > 4) {
+            response = "Sorry, you are limited to only 4 tries. Game over.";
+        } else {
+            String tooHighLowText;
+            if (guessedNumber < getRandomNumber()) {
+                tooHighLowText = "low";
+            } else {
+                tooHighLowText = "high";
+            }
+            String loseText = String.format("You didn't get it - you're too %s", tooHighLowText);
+            response = guessedNumber == getRandomNumber() ? winningMessage : loseText;
+        }
+
+        return response;
     }
 
     public int getRandomNumber() {
-        int num = (int) Math.ceil(Math.random() * 10);
-        return num;
+        return randomNumber;
     }
 }
--- src/test/com/adam/game/GuessingGameTest.java
+++ src/test/com/adam/game/GuessingGameTest.java
@@ -1,14 +1,14 @@
 package com.adam.game;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GuessingGameTest {
 
+    public static final int GAME_RANDOMNESS_TRIES = 100;
     private GuessingGame GAME;
 
     @BeforeEach
@@ -17,24 +17,23 @@ public class GuessingGameTest {
     }
 
     @Test
-    @Disabled
     public void testSimpleWin() {
         int randomNum = GAME.getRandomNumber();
         String message = GAME.guess(randomNum);
-        assertEquals("You got it", message);
+        assertEquals("You got it in 1 try", message);
     }
 
     @Test
     public void testOneWrongPositiveGuess() {
         int randomNum = GAME.getRandomNumber();
         String message = GAME.guess(randomNum + 1);
-        assertEquals("You didn't get it", message);
+        assertEquals("You didn't get it - you're too high", message);
     }
 
     @Test
     public void testOneWrongNegativeGuess() {
         String message = GAME.guess(-1);
-        assertEquals("You didn't get it", message);
+        assertEquals("You didn't get it - you're too low", message);
     }
 
     @Test
@@ -42,7 +41,9 @@ public class GuessingGameTest {
         //1 2 3 4 5 6 7 8 9 10
         //1 1 1 1 0 1 0 1 0 1 = 10
         int[] randomNumbersCount = new int[10];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < GAME_RANDOMNESS_TRIES; i++) {
+            //creating a local GAME object; to override -> use this.GAME
+            GuessingGame GAME = new GuessingGame();
             int randomNumber = GAME.getRandomNumber();
             randomNumbersCount[randomNumber - 1] = 1;
         }
@@ -51,8 +52,59 @@ public class GuessingGameTest {
         for (int i = 0; i < 10; i++) {
             sum += randomNumbersCount[i];
         }
-        System.out.println(sum);
 
         assertEquals(10, sum);
     }
+
+    @Test
+    public void testFourWrongGuesses() {
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        String lastGuessMessage = GAME.guess(-1);
+
+        assertEquals("You didn't get it and you've had 4 tries. Game over.", lastGuessMessage);
+    }
+
+    @Test
+    public void testThreeWrongGuessesAndOneCorrect() {
+        int correctAnswer = GAME.getRandomNumber();
+
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        String fourthGuessMessage = GAME.guess(correctAnswer);
+
+        assertEquals("You got it in 4 tries", fourthGuessMessage);
+    }
+
+    @Test
+    public void testTwoWrongGuessesAndOneCorrect() {
+        int correctAnswer = GAME.getRandomNumber();
+
+        GAME.guess(-1);
+        GAME.guess(-1);
+        String thirdGuessMessage = GAME.guess(correctAnswer);
+
+        assertTrue(thirdGuessMessage.contains("3"), "Should indicate 3 tries");
+        assertTrue(thirdGuessMessage.contains("You got it"), "Should indicate that we got the right number");
+    }
+
+    @Test
+    public void testTenWrongGuesses() {
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        GAME.guess(-1);
+        String lastGuessMessage = GAME.guess(-1);
+
+        assertEquals("Sorry, you are limited to only 4 tries. Game over.", lastGuessMessage);
+    }
+
+
 }
